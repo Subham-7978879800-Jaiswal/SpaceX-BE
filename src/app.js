74337f5d70ce51b22fc1397c192080360002f884
Subham <sc88dev0028@gmail.com>
2023-05-13 02:28:03 +0530

    Fixing cors

--- src/app.js
+++ src/app.js
@@ -9,11 +9,14 @@ const { LaunchesRouter } = require("./Routers/launches/Launches.router");
 
 const app = express();
 
-const corsOption = {
-  origin: ["http://3.110.86.75:3000"],
-};
-
-app.use(cors(corsOption));
+app.use(
+  cors({
+    origin: "http://3.110.86.75:4000", // Replace with your allowed origin
+    methods: ["GET", "POST"], // Specify the allowed HTTP methods
+    allowedHeaders: ["Content-Type"], // Specify the allowed headers
+    optionsSuccessStatus: 200,
+  })
+);      // Set the response status for preflight requests));
 app.use(morgan("combined"));
 
 app.use(express.json()); // ~* Middleware added, This middleware takes the request body, converts it to JSON and add it to req in the callback function
